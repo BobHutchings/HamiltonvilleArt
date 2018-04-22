59a913ea910ca1dcb83dc9fc162f7a1bb69b3526
Bob Hutchings <Bob@Bob.Bob>
2018-04-22 17:09:47 -0400

    First pass at Image Loader functionality.  Renamed a bunch of img files to remove spaces.

--- js/imageLoader.js
+++ js/imageLoader.js
@@ -1,4 +1,11 @@
 HamiltonvilleArt.ImageLoader = (function() {
+    var NAME = 'HamiltonvilleArt.ImageLoader';
+
+    // Image Cache
+    // POJO
+    // Each property corresponds to a Dir name on the server, containing a given set of image files.
+    // The value of each property is an Array of image file names. All of which have been cached by the browser.
+    var imgCache = Object.create(null);
 
     // Get ImageLoader configuration data
     var ajax = new HamiltonvilleArt.Ajax();
@@ -10,7 +17,44 @@ HamiltonvilleArt.ImageLoader = (function() {
 
     // Callback Function:  Load Artwork Images.
     function loadImages(reqObj) {
-        console.dir(reqObj);
+        if ((typeof reqObj.response === 'string') || (typeof reqObj.responseText === 'string')) {
+            var respStr = (typeof reqObj.response === 'string') ? reqObj.response : reqObj.responseText;
+
+            try {
+                var i, j;
+                var cfgData = JSON.parse(respStr);
+
+                // Build the Image Cache object.
+                for (i = 0; i < cfgData.artworkImgs.length; i++) {
+                    imgCache[ cfgData.artworkImgs[i].dirName ] = [];
+
+                    for (j = 0; j < cfgData.artworkImgs[i].files.length; j++) {
+                        var img = new Image();
+                        var path = 'assets/' + cfgData.artworkImgs[i].dirName + '/' + cfgData.artworkImgs[i].files[j];
+                        
+                        // Cache the image.
+                        imgCache[ cfgData.artworkImgs[i].dirName ].push(path);
+                        img.src = path;
+                    }
+                }
+
+                console.dir(imgCache);
+
+            } catch (err) {
+                HamiltonvilleArt.Log.write({
+                    obj: NAME,
+                    fun: 'loadImages',
+                    msg: 'ERROR. ' + err.message
+                });    
+            }
+        
+        } else {
+            HamiltonvilleArt.Log.write({
+                obj: NAME,
+                fun: 'loadImages',
+                msg: 'ERROR. Failed to retrieve Image Loader config data.'
+            });
+        }
     }
 
     // API Function: Get Images
