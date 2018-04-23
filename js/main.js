9fff8f28c8eb25e4438ea42f870a8e56c8e4e261
Bob Hutchings <Bob@Bob.Bob>
2018-04-22 20:11:42 -0400

    First pass at app initialization.

--- js/main.js
+++ js/main.js
@@ -3,5 +3,22 @@ var HamiltonvilleArt = {};
 
 // Initialize the app.
 (function() {
-    console.log('Hamiltonville Art Website initialized!');
+
+    // Figure out when the Image Loader has finished doing its thing
+    var interval = window.setInterval(
+        function() {
+            if (HamiltonvilleArt.ImageLoader) {
+                if (HamiltonvilleArt.ImageLoader.getReadyState()) {
+                    HamiltonvilleArt.Log.write({
+                        obj: 'Main',
+                        fun: 'Main',
+                        msg: 'Hamiltonville Art Website initialized!'
+                    });
+
+                    document.getElementById('hva_loading').style.display = "none";
+                    window.clearInterval(interval);
+                }
+            }
+        }, 100
+    );
 })();
