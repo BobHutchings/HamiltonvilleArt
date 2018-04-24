f7fb800947917d4aade590cf9d54ccd15dcfbc23
Bob Hutchings <Bob@Bob.Bob>
2018-04-23 21:41:55 -0400

    Nav is now being dynamically constructed.

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ var HamiltonvilleArt = {};
     // Figure out when the Image Loader has finished doing its thing
     var interval = window.setInterval(
         function() {
+            var imgCats;
+
             if (HamiltonvilleArt.ImageLoader) {
                 if (HamiltonvilleArt.ImageLoader.getReadyState()) {
                     HamiltonvilleArt.Log.write({
@@ -16,7 +18,13 @@ var HamiltonvilleArt = {};
                         msg: 'Hamiltonville Art Website initialized!'
                     });
 
+                    // Turn off the "loading" gif.
                     document.getElementById('hva_loading').style.display = "none";
+                    
+                    // Build the Nav using the available Image Categories.
+                    imgCats = HamiltonvilleArt.ImageLoader.getImgCats();
+                    HamiltonvilleArt.NavBuilder.buildNav(imgCats);
+
                     window.clearInterval(interval);
                 }
             }
--- js/navBuilder.js
+++ js/navBuilder.js
@@ -4,14 +4,37 @@ HamiltonvilleArt.NavBuilder = (function() {
     // Use the information gathered by the Image Loader, to build the Nav
     // Takes an array of strings, representing the differnt categories of artwork available in the nav.
     function buildNav(cats) {
-        let navElmt = document.getElementById('hva_main-nav');
+        var navElmt = document.getElementById('hva_main-nav');
+        var i;
 
         // Sanity check the function param.
         // We'll know it's an Array if the length is greater than 1
         // Within the context of this app, there *will* be more than 1 category of images to display.
         if ((typeof cats === 'object') && (cats.length > 1)) {
+            for (i = 0; i < cats.length; i++) {
+                var div = document.createElement('div');
+                var txt = document.createTextNode(cats[i]);
+                var aClass = document.createAttribute('class');
+                var aId = document.createAttribute('id');
 
+                aClass.value = 'hva_cat-link';
+                aId.value = 'hva_' + cats[i];
+
+                div.setAttributeNode(aClass);
+                div.setAttributeNode(aId);
+                div.appendChild(txt);
+
+                navElmt.appendChild(div);
+            }
+
+            // Click Event handler for the nav
+            navElmt.addEventListener('click', onNavClick);
         }
+    }
+
+    // The user has clicked a Nav link.
+    function onNavClick(evt) {
+        console.dir(evt);
     }
 
     // Public API
